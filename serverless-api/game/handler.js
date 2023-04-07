b04be03e60d1542f664b5352329beabca64d5923
mubin86 <rahman.mmubinur@gmail.com>
2023-04-08 01:12:20 +0600

    lambda exporting updated

--- serverless-api/game/handler.js
+++ serverless-api/game/handler.js
@@ -1,16 +1,13 @@
-export const gameHandler = (event, context, cb) => {
+exports.gameHandler = (event, context, cb) => {
     const p = new Promise((resolve) => {
       resolve('success');
     });
+
+    const message = 'Hello people, terraform poc game';
     const response = {
       statusCode: 200,
-      body: JSON.stringify(
-        {
-          message: 'Hello people',
-        },
-        null,
-        2
-      ),
+      body: JSON.stringify(message),
+      headers: {'Content-Type': 'application/json'}
     };
     p.then(() => cb(null, response)).catch((e) => cb(e));
   };
